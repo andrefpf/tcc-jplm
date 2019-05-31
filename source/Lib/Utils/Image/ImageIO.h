253bdf918d56f4bf54595d9fb6877e00e6c4914a
Ismael Seidel <ismaelseidel@gmail.com>
2019-05-31 13:20:46 -0300

    added new tests to PPMBinaryFileTests to ensure the image that is written is the same after the file is readed in

--- source/Lib/Utils/Image/ImageIO.h
+++ source/Lib/Utils/Image/ImageIO.h
@@ -21,23 +21,23 @@ void write_image_to_file(
 
 
 template<typename T>
-void write(const Image<T>& image, const std::string& filename) {
-  using fpath = std::filesystem::path;
+void write(const Image<T>& image, const std::string& filename,
+    const bool overwrite = false) {
+  namespace fs = std::filesystem;
+  using fpath = fs::path;
   auto name = fpath(filename);
 
+  if (!overwrite && fs::exists(filename)) {
+    throw ImageIOExceptions::FileAlreadyExistsException();
+  }
+
   if (name.extension() == fpath(".ppm")) {
-    std::cout << "is a ppm file :)" << std::endl;
-    // auto ppm_file = PPMBinaryFile(filename);
     auto max_value =
         static_cast<std::size_t>(std::pow(2.0, image.get_bpp())) - 1;
     auto oppened_image = PixelMapFileIO::open(filename, PixelMapType::P6,
         image.get_width(), image.get_height(), max_value);
-
-    std::cout << "Image type is: " << image.get_type() << std::endl;
     auto oppened_image_as_rgb = std::unique_ptr<PPMBinaryFile>(
         static_cast<PPMBinaryFile*>(oppened_image.release()));
-    std::cout << "raster begin " << oppened_image_as_rgb->get_raster_begin()
-              << std::endl;
     oppened_image_as_rgb->write_image_to_file(image);
   }
 }
@@ -49,14 +49,11 @@ void write(const Image<T>& patch_image, const std::string& filename,
   auto name = fpath(filename);
 
   if (name.extension() == fpath(".ppm")) {
-    std::cout << "is a ppm file :)" << std::endl;
     auto oppened_image =
         PixelMapFileIO::open(filename);  //should test if exists...
 
     auto oppened_image_as_rgb = std::unique_ptr<PPMBinaryFile>(
         static_cast<PPMBinaryFile*>(oppened_image.release()));
-    std::cout << "patch raster begin "
-              << oppened_image_as_rgb->get_raster_begin() << std::endl;
     oppened_image_as_rgb->write_image_patch_to_file(patch_image, origin);
   }
 }
--- source/Tests/Utils/Image/PPMBinaryFileTests.cpp
+++ source/Tests/Utils/Image/PPMBinaryFileTests.cpp
@@ -39,36 +39,160 @@
  */
 
 #include <iostream>
-#include "PixelMapFileIO.h"
+#include "ImageIO.h"
 #include "PPMBinaryFile.h"
+#include "PixelMapFileIO.h"
 #include "gtest/gtest.h"
 
 std::string resources_path = "../resources";
 
 
 TEST(InitialTest, IsTypeP6) {
-	auto file_original = PixelMapFileIO::open({resources_path + "/rgb_pattern/pattern.ppm"});
-	EXPECT_EQ(PixelMapType::P6,file_original->get_type());
+  auto file_original =
+      PixelMapFileIO::open({resources_path + "/rgb_pattern/pattern.ppm"});
+  EXPECT_EQ(PixelMapType::P6, file_original->get_type());
 }
 
 
 TEST(InitialTest, HasExpectedWidth) {
-	auto file_original = PixelMapFileIO::open({resources_path + "/rgb_pattern/pattern.ppm"});
-	EXPECT_EQ(13,file_original->get_width());
+  auto file_original =
+      PixelMapFileIO::open({resources_path + "/rgb_pattern/pattern.ppm"});
+  EXPECT_EQ(13, file_original->get_width());
 }
 
 
 TEST(InitialTest, HasExpectedHeight) {
-	auto file_original = PixelMapFileIO::open({resources_path + "/rgb_pattern/pattern.ppm"});
-	EXPECT_EQ(13,file_original->get_height());
+  auto file_original =
+      PixelMapFileIO::open({resources_path + "/rgb_pattern/pattern.ppm"});
+  EXPECT_EQ(13, file_original->get_height());
 }
 
 
 TEST(InitialTest, HasExpectedBpp) {
-	auto file_original = PixelMapFileIO::open({resources_path + "/rgb_pattern/pattern.ppm"});
-	EXPECT_EQ(10,file_original->get_number_of_bits_per_pixel());
+  auto file_original =
+      PixelMapFileIO::open({resources_path + "/rgb_pattern/pattern.ppm"});
+  EXPECT_EQ(10, file_original->get_number_of_bits_per_pixel());
 }
 
+uint16_t max_r_10_bit = 1023;
+uint16_t max_g_10_bit = 1023;
+uint16_t max_b_10_bit = 1023;
+
+uint16_t half_r_10_bit = 511;
+uint16_t half_g_10_bit = 511;
+uint16_t half_b_10_bit = 511;
+
+uint16_t min_r_10_bit = 0;
+uint16_t min_g_10_bit = 0;
+uint16_t min_b_10_bit = 0;
+
+auto yellow_10_bit = std::make_tuple(max_r_10_bit, max_g_10_bit, min_b_10_bit);
+auto red_10_bit = std::make_tuple(max_r_10_bit, min_g_10_bit, min_b_10_bit);
+auto green_10_bit = std::make_tuple(min_r_10_bit, max_g_10_bit, min_b_10_bit);
+auto blue_10_bit = std::make_tuple(min_r_10_bit, min_g_10_bit, max_b_10_bit);
+auto white_10_bit = std::make_tuple(max_r_10_bit, max_g_10_bit, max_b_10_bit);
+auto black_10_bit = std::make_tuple(min_r_10_bit, min_g_10_bit, min_b_10_bit);
+auto gray_10_bit = std::make_tuple(half_r_10_bit, half_g_10_bit, half_b_10_bit);
+
+
+struct PPMBinaryWriteTests : public testing::Test {
+ protected:
+  std::unique_ptr<RGBImage<uint16_t>> image =
+      std::make_unique<RGBImage<uint16_t>>(13, 13, 10);
+  const std::string output_filename =
+      std::string({resources_path + "/test.ppm"});
+  PPMBinaryWriteTests() {
+    for (auto i = 0; i < 13; ++i) {
+      for (auto j = 0; j < 13; ++j) {
+        if (i == 0 || i == 12 || j == 0 || j == 12) {
+          image->set_pixel_at(white_10_bit, i, j);
+          continue;
+        }
+        if (i == 1 || i == 11 || j == 1 || j == 11) {
+          image->set_pixel_at(blue_10_bit, i, j);
+          continue;
+        }
+        if (i == 2 || i == 10 || j == 2 || j == 10) {
+          image->set_pixel_at(green_10_bit, i, j);
+          continue;
+        }
+        if (i == 3 || i == 9 || j == 3 || j == 9) {
+          image->set_pixel_at(red_10_bit, i, j);
+          continue;
+        }
+        if (i == 4 || i == 8 || j == 4 || j == 8) {
+          image->set_pixel_at(black_10_bit, i, j);
+          continue;
+        }
+        if (i == 5 || i == 7 || j == 5 || j == 7) {
+          image->set_pixel_at(gray_10_bit, i, j);
+          continue;
+        }
+        image->set_pixel_at(yellow_10_bit, i, j);
+      }
+    }
+  }
+
+  ~PPMBinaryWriteTests() {
+  	namespace fs = std::filesystem;
+    if (fs::exists(output_filename)) {
+      fs::remove(output_filename);
+    }
+  }
+};
+
+
+TEST_F(PPMBinaryWriteTests, ImageWriteCreatesAFile) {
+  namespace fs = std::filesystem;
+  if (fs::exists(output_filename)) {
+    fs::remove(output_filename);
+  }
+  EXPECT_FALSE(fs::exists(output_filename));
+  ImageIO::write(*(image.get()), output_filename);
+  EXPECT_TRUE(fs::exists(output_filename));
+}
+
+
+TEST_F(PPMBinaryWriteTests, ImageWriteOverExistingFileThrows) {
+  namespace fs = std::filesystem;
+  if (!fs::exists(output_filename)) {
+    ImageIO::write(*(image.get()), output_filename);
+    EXPECT_TRUE(fs::exists(output_filename));
+  }
+
+  EXPECT_THROW(ImageIO::write(*(image.get()), output_filename),
+      ImageIOExceptions::FileAlreadyExistsException);
+}
+
+
+TEST_F(PPMBinaryWriteTests, ImageWriteCreatesAFileThatCanBeOppened) {
+  namespace fs = std::filesystem;
+  if (fs::exists(output_filename)) {
+    fs::remove(output_filename);
+  }
+  EXPECT_FALSE(fs::exists(output_filename));
+  ImageIO::write(*(image.get()), output_filename);
+  auto ppm_file = PixelMapFileIO::open(output_filename);
+  EXPECT_EQ(ppm_file->get_type(), PixelMapType::P6);
+  fs::remove(output_filename);
+}
+
+
+TEST_F(PPMBinaryWriteTests, ImageWriteCreatesAFileThatContatinsSameDataAsTheWrittenImage) {
+  namespace fs = std::filesystem;
+  if (fs::exists(output_filename)) {
+    fs::remove(output_filename);
+  }
+  EXPECT_FALSE(fs::exists(output_filename));
+  ImageIO::write(*(image.get()), output_filename);
+  auto ppm_file = PixelMapFileIO::open(output_filename);
+
+  auto readed_image = ppm_file->read_full_image();
+  ASSERT_EQ(readed_image.index(), 1);
+  auto readed_16bpp_image = std::move(std::get<1>(readed_image));
+  EXPECT_EQ(*(readed_16bpp_image.get()), *(image.get()));
+  fs::remove(output_filename);
+}
 
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest(&argc, argv);
