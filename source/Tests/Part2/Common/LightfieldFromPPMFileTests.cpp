3177c58a23ae2922391a25ac0b1b3ccdbd7a7638
Ismael Seidel <ismaelseidel@gmail.com>
2019-06-10 13:03:47 -0300

    included six tests to lightfield from ppm file

--- source/Tests/Part2/Common/LightfieldFromPPMFileTests.cpp
+++ source/Tests/Part2/Common/LightfieldFromPPMFileTests.cpp
@@ -2,9 +2,8 @@
 * @Author: Ismael Seidel
 * @Date:   2019-06-06 15:26:52
 * @Last Modified by:   Ismael Seidel
-* @Last Modified time: 2019-06-10 12:28:07
+* @Last Modified time: 2019-06-10 13:03:24
 */
-
 
 
 #include <iostream>
@@ -15,20 +14,60 @@
 std::string resources_path = "../resources";
 
 
-TEST(ViewFromPPMFileIntantiation, Test) {
-	LightfieldDimension<std::size_t> size(3,3,32,32);
-	LightfieldCoordinate<std::size_t> initial(0,0,0,0);
+struct LFFromPPMSmallGreek : public testing::Test {
+ protected:
+  LightfieldDimension<std::size_t> size;
+  LightfieldCoordinate<std::size_t> initial;
+  LightfieldFromPPMFile<uint16_t> lightfield;
 
-	LightfieldIOConfiguration lfc(resources_path+"/small_greek/", initial, size);
-	// EXPECT_EQ(lfc.get_size(), size);
-	auto lf = LightfieldFromPPMFile<uint16_t>(lfc);
-	// lf.
+ public:
+  LFFromPPMSmallGreek()
+      : size(3, 3, 32, 32), initial(0, 0, 0, 0),
+        lightfield({{resources_path + "/small_greek/"}, initial, size}){};
+  ~LFFromPPMSmallGreek() = default;
+};
+
+
+TEST_F(LFFromPPMSmallGreek, HasCorrectWidth) {
+  auto width = lightfield.get_width();
+  EXPECT_EQ(width, 3);
+}
+
+
+TEST_F(LFFromPPMSmallGreek, HasCorrectHeight) {
+  auto height = lightfield.get_height();
+  EXPECT_EQ(height, 3);
+}
+
+
+TEST_F(LFFromPPMSmallGreek, HasCorrectViewWidth) {
+  auto width = lightfield.get_views_width();
+  EXPECT_EQ(width, 32);
+}
+
+
+TEST_F(LFFromPPMSmallGreek, HasCorrectViewHeight) {
+  auto height = lightfield.get_views_height();
+  EXPECT_EQ(height, 32);
+}
+
+
+TEST_F(LFFromPPMSmallGreek, HasCorrectViewBpp) {
+  //notice that there is no bpp informed
+  auto bpp = lightfield.get_views_bpp();
+  EXPECT_EQ(bpp, 10);
+}
+
+
+TEST_F(LFFromPPMSmallGreek, HasCorrectDimensions) {
+  auto dimensions = lightfield.get_dimensions();
+  EXPECT_EQ(dimensions, size);
 }
 
 
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest(&argc, argv);
-   if (argc > 1) {
+  if (argc > 1) {
     resources_path = std::string(argv[1]);
   }
   return RUN_ALL_TESTS();
