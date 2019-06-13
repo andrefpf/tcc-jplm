44713bb4968ecbd308a4b126fc6cd19be6ea0241
Ismael Seidel <ismaelseidel@gmail.com>
2019-06-13 15:04:00 -0300

    WIP: including YCoCg conversion

--- source/Lib/Utils/Image/Image.h
+++ source/Lib/Utils/Image/Image.h
@@ -708,4 +708,59 @@ class GrayScaleImage : public Image<T> {
   }
 };
 
+
+template<typename T>
+class YCoCgImage : public ThreeChannelImage<T> {
+ public:
+  YCoCgImage(
+      std::size_t width, std::size_t height, std::size_t bpp, ImageType type)
+      : ThreeChannelImage<T>(width, height, bpp, type){};
+
+
+  YCoCgImage(YCoCgImage<T>&& other) noexcept
+      : ThreeChannelImage<T>(std::move(other)) {
+  }
+
+
+  YCoCgImage(const YCoCgImage<T>& other) : ThreeChannelImage<T>(other){};
+
+
+  YCoCgImage& operator=(YCoCgImage<T>&& other) {
+    ThreeChannelImage<T>::operator=(std::move(other));
+    return *this;
+  }
+
+
+  inline bool operator==(const YCoCgImage<T>& other) const {
+    return this->is_equal(other);
+  }
+
+
+  ~YCoCgImage() = default;
+
+
+  std::vector<std::string> get_channel_names() const final {
+    return {"Y", "Co", "Cg"};
+  }
+
+
+  auto begin() {
+    return this->channels.begin();
+  }
+
+
+  auto end() {
+    return this->channels.end();
+  }
+
+
+  auto cbegin() const noexcept {
+    return this->channels.cbegin();
+  }
+
+
+  auto cend() const noexcept {
+    return this->channels.cend();
+  }
+};
 #endif /* end of include guard: JPLM_LIB_UTILS_IMAGE_IMAGE_H__ */
