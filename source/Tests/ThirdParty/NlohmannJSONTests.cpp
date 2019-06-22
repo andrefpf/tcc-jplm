03a9fa54a9a59404f83c8424dd7fc5eca8a491da
Pedro Garcia <pdr.grc.frts@gmail.com>
2019-06-21 21:19:05 -0300

    String to json and inverse serialization added to NlohmannJSONTests.

--- source/Tests/ThirdParty/NlohmannJSONTests.cpp
+++ source/Tests/ThirdParty/NlohmannJSONTests.cpp
@@ -238,12 +238,61 @@ TEST_F(SimpleMaxSizeNlohmannJSONTest, TestFloatMaxsize) {
 }
 
 TEST(SimpleTest, Iterator) {
-  json j = { {"A", 1}, {"B", 2} };
+  json j = {{"A", 1}, {"B", 2}};
   int count = 1;
-  for (auto& i : j.items())
-    i.value() = ++count;
-  EXPECT_EQ(j, json({ {"A", 2}, {"B", 3} }));
+  for (auto& i : j.items()) i.value() = ++count;
+  EXPECT_EQ(j, json({{"A", 2}, {"B", 3}}));
 }
+
+TEST(SimpleTest, JSONAsFirstClassDataType) {
+  const json expected = {{"pi", 3.141}, {"happy", true}, {"name", "Niels"},
+      {"nothing", nullptr}, {"answer", {{"everything", 42}}},
+      {"list", {1, 0, 2}}, {"object", {{"currency", "USD"}, {"value", 42.99}}}};
+  json j;
+  j["pi"] = 3.141;
+  j["happy"] = true;
+  j["name"] = "Niels";
+  j["nothing"] = nullptr;
+  j["answer"]["everything"] = 42;
+  j["list"] = {1, 0, 2};
+  j["object"] = {{"currency", "USD"}, {"value", 42.99}};
+  EXPECT_EQ(j, expected);
+}
+
+TEST(SimpleTest, SerializeDeserialize) {
+  const json expected = "{ \"happy\": true, \"pi\": 3.141 }"_json;
+  const json raw_literal = R"(
+  {
+    "happy": true,
+    "pi": 3.141
+  }
+  )"_json;
+  auto explicitely_parsed = json::parse("{ \"happy\": true, \"pi\": 3.141 }");
+  EXPECT_EQ(raw_literal, expected);
+  EXPECT_EQ(explicitely_parsed, expected);
+}
+
+TEST(SimpleTest, SerializeDumpDeserialize) {
+  const json expected = "{ \"happy\": true, \"pi\": 3.141 }"_json;
+  const std::string dumped = expected.dump();  // {\"happy\":true,\"pi\":3.141}
+  const json restored = json::parse(dumped);
+  const json restored_notparsed = json(dumped);
+  EXPECT_EQ(restored, expected);
+  EXPECT_NE(restored, restored_notparsed);
+}
+
+TEST(SimpleTest, SerializationVersusAssigment) {
+  const json j_string = "this is a JSON string";
+  std::string cpp_string = j_string.get<std::string>();
+  std::string cpp_string2;
+  j_string.get_to(cpp_string2);
+  EXPECT_EQ(cpp_string, cpp_string2);
+  EXPECT_EQ(cpp_string2, j_string.get<std::string>());
+  std::string serialized_string = j_string.dump();
+  EXPECT_NE(cpp_string, serialized_string);
+  EXPECT_NE(j_string, serialized_string);
+}
+
 
 int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
