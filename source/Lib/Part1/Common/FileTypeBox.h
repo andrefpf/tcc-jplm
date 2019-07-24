634eb824114e0ac2d991743188d627cccaa124a9
Ismael Seidel <ismaelseidel@gmail.com>
2019-07-24 10:31:48 -0300

    added FileTypeBox

--- source/Lib/Part1/Common/FileTypeBox.h
+++ source/Lib/Part1/Common/FileTypeBox.h
@@ -31,61 +31,17 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/** \file     JpegPlenoSignatureBoxTest.cpp
+/** \file     FileTypeBox.cpp
  *  \brief    
  *  \details  
  *  \author   Ismael Seidel <i.seidel@samsung.com>
- *  \date     2019-07-23
+ *  \date     2019-07-24
  */
 
+#ifndef JPLM_LIB_PART1_COMMON_FILETYPEBOX_H__
+#define JPLM_LIB_PART1_COMMON_FILETYPEBOX_H__
 
-#include <iostream>
-#include "Lib/Part1/Common/JpegPlenoSignatureBox.h"
-#include "gtest/gtest.h"
+#include "Box.h"
+#include "DefinedBoxes.h"
 
-
-TEST(JpegPlenoSignatureBoxBasic, Initialization) {
-	EXPECT_NO_THROW(JpegPlenoSignatureBox());
-}
-
-
-TEST(JpegPlenoSignatureBoxBasic, CorrectLBoxValue) {
-	auto pleno_signature_box = JpegPlenoSignatureBox();
-	EXPECT_EQ(pleno_signature_box.get_lbox().get_value(), 12);
-}
-
-
-TEST(JpegPlenoSignatureBoxBasic, XLBoxDoNotExist) {
-	auto pleno_signature_box = JpegPlenoSignatureBox();
-	EXPECT_FALSE(pleno_signature_box.get_xlbox());
-}
-
-
-TEST(JpegPlenoSignatureBoxBasic, TBoxValueNotOne) {
-	auto pleno_signature_box = JpegPlenoSignatureBox();
-	EXPECT_NE(pleno_signature_box.get_tbox().get_value(), 1);
-}
-
-
-TEST(JpegPlenoSignatureBoxBasic, CorrectTBoxValue) {
-	auto pleno_signature_box = JpegPlenoSignatureBox();
-	EXPECT_EQ(pleno_signature_box.get_tbox().get_value(), 0x6a706c20);
-}
-
-
-TEST(JpegPlenoSignatureBoxBasic, CorrectDBoxValue) {
-	auto pleno_signature_box = JpegPlenoSignatureBox();
-	auto dbox = pleno_signature_box.get_dbox();
-	auto contents = dbox->get_ref_to_contents();
-	auto& vec = std::any_cast<std::vector<unsigned char>&>(contents);
-	EXPECT_EQ(vec.at(0), 0x0d);
-	EXPECT_EQ(vec.at(1), 0x0a);
-	EXPECT_EQ(vec.at(2), 0x87);
-	EXPECT_EQ(vec.at(3), 0x0a);
-}
-
-
-int main(int argc, char *argv[]) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+#endif /* end of include guard: JPLM_LIB_PART1_COMMON_FILETYPEBOX_H__ */
--- source/Tests/Part1/Common/FileTypeBoxTests.cpp
+++ source/Tests/Part1/Common/FileTypeBoxTests.cpp
@@ -31,58 +31,18 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/** \file     JpegPlenoSignatureBoxTest.cpp
+/** \file     FileTypeBoxTests.cpp
  *  \brief    
  *  \details  
  *  \author   Ismael Seidel <i.seidel@samsung.com>
- *  \date     2019-07-23
+ *  \date     2019-07-24
  */
 
 
 #include <iostream>
-#include "Lib/Part1/Common/JpegPlenoSignatureBox.h"
+#include "Lib/Part1/Common/FileTypeBox.h"
 #include "gtest/gtest.h"
 
-
-TEST(JpegPlenoSignatureBoxBasic, Initialization) {
-	EXPECT_NO_THROW(JpegPlenoSignatureBox());
-}
-
-
-TEST(JpegPlenoSignatureBoxBasic, CorrectLBoxValue) {
-	auto pleno_signature_box = JpegPlenoSignatureBox();
-	EXPECT_EQ(pleno_signature_box.get_lbox().get_value(), 12);
-}
-
-
-TEST(JpegPlenoSignatureBoxBasic, XLBoxDoNotExist) {
-	auto pleno_signature_box = JpegPlenoSignatureBox();
-	EXPECT_FALSE(pleno_signature_box.get_xlbox());
-}
-
-
-TEST(JpegPlenoSignatureBoxBasic, TBoxValueNotOne) {
-	auto pleno_signature_box = JpegPlenoSignatureBox();
-	EXPECT_NE(pleno_signature_box.get_tbox().get_value(), 1);
-}
-
-
-TEST(JpegPlenoSignatureBoxBasic, CorrectTBoxValue) {
-	auto pleno_signature_box = JpegPlenoSignatureBox();
-	EXPECT_EQ(pleno_signature_box.get_tbox().get_value(), 0x6a706c20);
-}
-
-
-TEST(JpegPlenoSignatureBoxBasic, CorrectDBoxValue) {
-	auto pleno_signature_box = JpegPlenoSignatureBox();
-	auto dbox = pleno_signature_box.get_dbox();
-	auto contents = dbox->get_ref_to_contents();
-	auto& vec = std::any_cast<std::vector<unsigned char>&>(contents);
-	EXPECT_EQ(vec.at(0), 0x0d);
-	EXPECT_EQ(vec.at(1), 0x0a);
-	EXPECT_EQ(vec.at(2), 0x87);
-	EXPECT_EQ(vec.at(3), 0x0a);
-}
 
 
 int main(int argc, char *argv[]) {
