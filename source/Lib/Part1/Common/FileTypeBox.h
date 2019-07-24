7e90d07e998616b8741ed39cedc627f5aac05480
Ismael Seidel <ismaelseidel@gmail.com>
2019-07-24 12:49:13 -0300

    initial dev of JpegPlenoHeaderBox

--- source/Lib/Part1/Common/FileTypeBox.h
+++ source/Lib/Part1/Common/FileTypeBox.h
@@ -95,20 +95,18 @@ class FileTypeContents {
 
 
 class JpegPlenoFileTypeContents : public FileTypeContents {
-public:
-	JpegPlenoFileTypeContents(uint32_t minor_version = 0, std::vector<uint32_t> compatibility_list = {})
-      : FileTypeContents(
-      	static_cast<DefinedBoxesTypesUnderlyingType>(
-          DefinedBoxesTypes::JPEGPlenoSignatureBoxType),
-      	   minor_version,
-      	   compatibility_list
-      	) {
-      	//A file that conforms to this international standard shall have at least one CLi
-      	//filed and shall contain the value 'jpl\040' in one of the CLiFields
-      	if(!this->is_the_file_compatible_with(this->get_brand())) {
-      		this->add_compatible_standard_to_list(this->get_brand());
-      	}
-      }
+ public:
+  JpegPlenoFileTypeContents(
+      uint32_t minor_version = 0, std::vector<uint32_t> compatibility_list = {})
+      : FileTypeContents(static_cast<DefinedBoxesTypesUnderlyingType>(
+                             DefinedBoxesTypes::JPEGPlenoSignatureBoxType),
+            minor_version, compatibility_list) {
+    //A file that conforms to this international standard shall have at least one CLi
+    //filed and shall contain the value 'jpl\040' in one of the CLiFields
+    if (!this->is_the_file_compatible_with(this->get_brand())) {
+      this->add_compatible_standard_to_list(this->get_brand());
+    }
+  }
 };
 
 
@@ -117,6 +115,7 @@ class FileTypeDBox : public DBox {
   FileTypeDBox(const FileTypeContents& contents)
       : DBox(std::make_any<FileTypeContents>(contents)) {
   }
+
 
   FileTypeDBox(const FileTypeDBox& other)
       : DBox(std::make_any<FileTypeContents>(
@@ -139,7 +138,6 @@ class FileTypeDBox : public DBox {
 
 class FileTypeBox : public Box {
  public:
-
   FileTypeBox(const FileTypeContents& contents)
       : Box(TBox(static_cast<DefinedBoxesTypesUnderlyingType>(
                 DefinedBoxesTypes::FileTypeBoxType)),
