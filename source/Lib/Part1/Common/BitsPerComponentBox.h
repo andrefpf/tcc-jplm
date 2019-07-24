a27711a5e0f5bf6185d2ebf5b4d6fe2fd1f32da4
Ismael Seidel <ismaelseidel@gmail.com>
2019-07-24 15:25:01 -0300

    added BitsPerComponentBox

--- source/Lib/Part1/Common/BitsPerComponentBox.h
+++ source/Lib/Part1/Common/BitsPerComponentBox.h
@@ -31,87 +31,28 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/** \file     ImageHeaderBox.h
+/** \file     BitsPerComponentBox.h
  *  \brief    
  *  \details  
  *  \author   Ismael Seidel <i.seidel@samsung.com>
  *  \date     2019-07-24
  */
 
-#ifndef JPLM_LIB_PART1_COMMON_IMAGEHEADERBOX_H__
-#define JPLM_LIB_PART1_COMMON_IMAGEHEADERBOX_H__
+#ifndef JPLM_LIB_PART1_COMMON_BITSPERCOMPONENTBOX_H__
+#define JPLM_LIB_PART1_COMMON_BITSPERCOMPONENTBOX_H__
 
 #include "Box.h"
 #include "DefinedBoxes.h"
 
-
-enum class CoderTypeC : uint8_t {
-	JPEG_2000=0,
-	JPEG=1,
-	JPEG_LS=2,
-	JPEG_XT=3,
-	JPEG_XR=4,
-	JPEG_XS=5
-};
-
-
-typedef std::underlying_type<CoderTypeC>::type CoderTypeCUnderlyingType;
-
-
-class ImageHeaderContents
-{
-protected:
-	uint32_t height;
-	uint32_t width;
-	uint16_t nc;
-	uint8_t bpc;
-	CoderTypeC c;
-	uint8_t UnkC;
-	uint8_t IPR;
-public:
-	ImageHeaderContents();
-	~ImageHeaderContents();
-
-	uint64_t get_size() const noexcept {
-		return 2*sizeof(uint32_t)+sizeof(uint16_t)+3*sizeof(uint8_t)+sizeof(CoderTypeCUnderlyingType);
-	}
-};
-
-
-class ImageHeaderDBox : public DBox {
+class BitsPerComponentBox : public Box {
  public:
-  ImageHeaderDBox(const ImageHeaderContents& contents)
-      : DBox(std::make_any<ImageHeaderContents>(contents)) {
-  }
-
-
-  ImageHeaderDBox(const ImageHeaderDBox& other)
-      : DBox(std::make_any<ImageHeaderContents>(
-            std::any_cast<ImageHeaderContents>(other.contents))) {
-  }
-
-
-  ~ImageHeaderDBox() = default;
-
-
-  uint64_t get_size() const noexcept override {
-    return std::any_cast<ImageHeaderContents>(this->contents).get_size();
-  }
-
-
-  ImageHeaderDBox* clone() const override {
-    return new ImageHeaderDBox(*this);
-  }
-};
-
-
-class ImageHeaderBox : public Box {
- public:
-  ImageHeaderBox(const ImageHeaderDBox& contents)
+  BitsPerComponentBox(const std::vector<uint8_t>& bits_per_component_vector)
       : Box(TBox(static_cast<DefinedBoxesTypesUnderlyingType>(
-                DefinedBoxesTypes::ImageHeaderBoxType)),
-            contents){};
-  ~ImageHeaderBox() = default;
+                DefinedBoxesTypes::BitsPerComponentBoxType)),
+            CharArrayDBox(bits_per_component_vector)){};
+  ~BitsPerComponentBox() = default;
 };
 
-#endif /* end of include guard: JPLM_LIB_PART1_COMMON_IMAGEHEADERBOX_H__ */
+
+
+#endif /* end of include guard: JPLM_LIB_PART1_COMMON_BITSPERCOMPONENTBOX_H__ */
