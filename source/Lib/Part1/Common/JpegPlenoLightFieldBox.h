5d25fa2d3bc50f482fb03ed767d0448310a21b51
Ismael Seidel <ismaelseidel@gmail.com>
2019-07-25 11:04:17 -0300

    added JpegPlenoLightFieldBox

---
+++ source/Lib/Part1/Common/JpegPlenoLightFieldBox.h
@@ -0,0 +1,18 @@
+#ifndef JPEGPLENOLIGHTFIELDBOX_H__
+#define JPEGPLENOLIGHTFIELDBOX_H__
+
+#include "JpegPlenoCodestreamBox.h"
+
+
+//this must be moved to Part2/Common
+
+
+class JpegPlenoLightFieldBox : public JpegPlenoCodestreamBox
+{
+public:
+	JpegPlenoLightFieldBox();
+	~JpegPlenoLightFieldBox();
+	
+};
+
+#endif /* end of include guard: JPEGPLENOLIGHTFIELDBOX_H__ */
