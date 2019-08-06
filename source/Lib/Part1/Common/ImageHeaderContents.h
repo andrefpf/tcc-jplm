2633e307d2de7d6cb2b87b95a87fc6cfcd0eb1f1
Ismael Seidel <ismaelseidel@gmail.com>
2019-08-06 12:28:26 -0300

    WIP: refactoring. Changed ImageHeaderDBox

---
+++ source/Lib/Part1/Common/ImageHeaderContents.h
@@ -0,0 +1,69 @@
+#ifndef IMAGEHEADERCONTENTS_H__
+#define IMAGEHEADERCONTENTS_H__
+
+#include <tuple>  //std::tie
+#include "InMemoryDBoxContents.h"
+
+enum class CoderTypeC : uint8_t {
+  JPEG_2000 = 0,
+  JPEG = 1,
+  JPEG_LS = 2,
+  JPEG_XT = 3,
+  JPEG_XR = 4,
+  JPEG_XS = 5
+};
+
+
+typedef std::underlying_type<CoderTypeC>::type CoderTypeCUnderlyingType;
+
+
+class ImageHeaderContents : public InMemoryDBoxContents {
+ protected:
+  uint32_t height;
+  uint32_t width;
+  uint16_t nc;
+  uint8_t bpc;
+  CoderTypeC c;
+  uint8_t UnkC;
+  uint8_t IPR;
+
+ public:
+  ImageHeaderContents() = default;
+
+
+  virtual ImageHeaderContents* clone() const override {
+    return new ImageHeaderContents(*this);
+  }
+
+
+  virtual ~ImageHeaderContents() = default;
+
+
+  virtual uint64_t size() const noexcept override {
+    return 2 * sizeof(uint32_t) + sizeof(uint16_t) + 3 * sizeof(uint8_t) +
+           sizeof(CoderTypeCUnderlyingType);
+  }
+
+
+  virtual bool is_equal(const DBoxContents& other) const override {
+    if (typeid(*this) != typeid(other))
+      return false;
+    const auto& cast_other = dynamic_cast<const ImageHeaderContents&>(other);
+    return *this == cast_other;
+  }
+
+
+  bool operator==(const ImageHeaderContents& other) const {
+    return std::tie(this->height, this->width, this->nc, this->bpc, this->c,
+               this->UnkC, this->IPR) == std::tie(other.height, other.width,
+                                             other.nc, other.bpc, other.c,
+                                             other.UnkC, other.IPR);
+  }
+
+
+  bool operator!=(const ImageHeaderContents& other) const {
+    return !this->operator==(other);
+  }
+};
+
+#endif /* end of include guard: IMAGEHEADERCONTENTS_H__ */
---
+++ source/Lib/Part1/Common/ImageHeaderDBox.h
@@ -0,0 +1,28 @@
+#ifndef IMAGEHEADERDBOX_H__
+#define IMAGEHEADERDBOX_H__
+
+#include "DBox.h"
+#include "ImageHeaderContents.h"
+
+class ImageHeaderDBox : public DBox {
+ public:
+  ImageHeaderDBox(const ImageHeaderContents& contents)
+      : DBox(std::make_unique<ImageHeaderContents>(contents)) {
+  }
+
+
+  ImageHeaderDBox(const ImageHeaderDBox& other)
+      : DBox(std::make_unique<ImageHeaderContents>(
+            other.get_ref_to_contents())) {
+  }
+
+
+  ~ImageHeaderDBox() = default;
+
+
+  ImageHeaderDBox* clone() const override {
+    return new ImageHeaderDBox(*this);
+  }
+};
+
+#endif /* end of include guard: IMAGEHEADERDBOX_H__ */
