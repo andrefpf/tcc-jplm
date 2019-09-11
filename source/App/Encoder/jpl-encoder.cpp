eb42d0518d0caca8e07232315c1c57ed4fe4bb52
Ismael Seidel <ismaelseidel@gmail.com>
2019-09-11 16:24:51 -0300

    included initial version of jpl-encoder.cpp in app

--- source/App/Encoder/jpl-encoder.cpp
+++ source/App/Encoder/jpl-encoder.cpp
@@ -1,5 +1,22 @@
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <string>
+#include "Lib/Part2/Encoder/JPLM4DTransformModeLightFieldEncoder.h"
 
-int main() {
-  return 0;
+
+int main(int argc, char const* argv[]) {
+  auto configuration = ConfigurationFactory::get_configuration(argc, argv);
+  std::ofstream of_stream(
+      configuration->get_output_filename(), std::ofstream::binary);
+  if (!of_stream.is_open()) {
+    std::cerr << "Error opening output file" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  auto encoder = CodecFactory::get_encoder(std::move(configuration));
+  encoder.run();
+  const auto& jpl_file = encoder.get_ref_to_jpl_file();
+  of_stream << jpl_file;
+
+  exit(EXIT_SUCCESS);
 }
