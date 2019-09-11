1557611310b79e77e8c0316cbf3e8e906216932d
Ismael Seidel <ismaelseidel@gmail.com>
2019-09-11 16:28:36 -0300

    included initial version of jpl-decoder.cpp in app

--- source/App/Decoder/jpl-decoder.cpp
+++ source/App/Decoder/jpl-decoder.cpp
@@ -1,5 +1,13 @@
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <string>
+#include "Lib/Part2/Encoder/JPLM4DTransformModeLightFieldEncoder.h"
 
-int main() {
-  return 0;
+
+int main(int argc, char const* argv[]) {
+  auto configuration = ConfigurationFactory::get_configuration(argc, argv);
+  auto decoder = CodecFactory::get_decoder(std::move(configuration));
+  decoder.run();
+  exit(EXIT_SUCCESS);
 }
