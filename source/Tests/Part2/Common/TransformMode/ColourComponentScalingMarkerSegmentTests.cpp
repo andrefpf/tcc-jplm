d6866a177f366d96403040ffd91a937b2a6c4307
Ismael Seidel <ismaelseidel@gmail.com>
2020-01-07 14:43:18 -0300

    colour component scaling marker returns correct infered option

--- source/Tests/Part2/Common/TransformMode/ColourComponentScalingMarkerSegmentTests.cpp
+++ source/Tests/Part2/Common/TransformMode/ColourComponentScalingMarkerSegmentTests.cpp
@@ -169,6 +169,27 @@ TEST(ContigousCodestreamAndParserTest, ParserDoesNotThrow) {
 }
 
 
+TEST(ContigousCodestreamAndParserTest,
+    ParserCorrectAlternativeForTheNumberOfColourComponents) {
+  std::string filename(
+      resources_path + "/markers/colour_component_scaling_marker.bin");
+  std::ifstream if_stream(filename, std::ifstream::binary);
+
+  auto managed_stream = ManagedStream(if_stream, 8);
+  auto contiguous_codestream =
+      ContiguousCodestreamCodeInMemory(managed_stream.get_n_bytes(8));
+  EXPECT_EQ(contiguous_codestream.get_next_byte(), std::byte(0xFF));
+  EXPECT_EQ(contiguous_codestream.get_next_byte(), std::byte(0xA2));
+
+
+  auto colour_component_scaling_marker_segment =
+      ColourComponentScalingMarkerSegmentParser::
+          get_colour_component_scaling_marker_segment(contiguous_codestream);
+  EXPECT_FALSE(colour_component_scaling_marker_segment
+                   .has_more_than_256_colour_components());
+}
+
+
 int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
   //this is to enable ctest to run the test passing the path to the resources
