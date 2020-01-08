4e854afa1041b23905e941baa9a1ff34c5314980
Ismael Seidel <ismaelseidel@gmail.com>
2020-01-08 13:09:48 -0300

    added parsing of optional boxes in the jplm lightfield transform mode decoder

--- source/Lib/Part2/Common/TransformMode/JPLM4DTransformModeLightFieldCodec.h
+++ source/Lib/Part2/Common/TransformMode/JPLM4DTransformModeLightFieldCodec.h
@@ -155,7 +155,7 @@ class JPLM4DTransformModeLightFieldCodec
                 (boder_blocks_policy == BorderBlocksPolicy::truncate)
                     ? size_shrink
                     : size_padding;
-            std::cout << "Shrink? " << (boder_blocks_policy == BorderBlocksPolicy::truncate) << std::endl;
+            // std::cout << "Shrink? " << (boder_blocks_policy == BorderBlocksPolicy::truncate) << std::endl;
             // std::cout << "size_shrink " << used_size_t << ", " << used_size_s << ", " << used_size_v << ", " << used_size_u << std::endl;
             // std::cout << "size_shrink " << used_size_t << ", " << used_size_s << ", " << used_size_v << ", " << used_size_u << std::endl;
             // std::cout << "Used size " << size.get_t() << ", " << size.get_s() << ", " << size.get_v() << ", " << size.get_u() << std::endl;
