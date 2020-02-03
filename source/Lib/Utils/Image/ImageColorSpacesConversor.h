c3572634def7a61ef4540fc2c8fff6bb10997f6d
Ismael Seidel <ismaelseidel@gmail.com>
2020-02-03 11:46:43 -0300

    removed views from PPM and tests related to views based on PPM files...

--- source/Lib/Utils/Image/ImageColorSpacesConversor.h
+++ source/Lib/Utils/Image/ImageColorSpacesConversor.h
@@ -217,12 +217,12 @@ ImageOut<T> to(const ImageIn<T>& source) {
   static_assert(std::is_base_of<Image<T>, ImageOut<T>>::value,
       "The output for conversion must be an image...");
 
-  if constexpr(std::is_same_v<Image<T>, ImageIn<T>>) {
+  if constexpr (std::is_same_v<Image<T>, ImageIn<T>>) {
     switch (source.get_type()) {
-      case ImageType::RGB : {
+      case ImageType::RGB: {
         return to<ImageOut>(dynamic_cast<const RGBImage<T>&>(source));
       }
-      case ImageType::BT601 : {
+      case ImageType::BT601: {
         return to<ImageOut>(dynamic_cast<const BT601Image<T>&>(source));
       }
     }
@@ -267,8 +267,7 @@ ImageOut<T> to(const ImageIn<T>& source) {
 
     default:
       std::cerr << "Conversion from this type is not yet supported..."
-      			<< "Type is: " << source.get_type() 
-                << std::endl;
+                << "Type is: " << source.get_type() << std::endl;
       exit(2);
       //! \todo change this exit by an exception in ImageColorSpacesConversor
   }
@@ -283,7 +282,6 @@ std::unique_ptr<ImageOut<T>> to(const std::unique_ptr<ImageIn<T>>& source) {
   auto out_image = to<ImageOut>(*(source.get()));
   return std::make_unique<ImageOut<T>>(std::move(out_image));
 }
-
 
 
 template<template<typename> class ImageOut, template<typename> class ImageIn,
--- source/Tests/Part2/Common/LightfieldFromFileTests.cpp
+++ source/Tests/Part2/Common/LightfieldFromFileTests.cpp
@@ -47,117 +47,122 @@
 std::string resources_path = "../resources";
 
 
-struct LFFromPPMSmallGreek : public testing::Test {
- protected:
-  LightfieldDimension<std::size_t> size;
-  LightfieldCoordinate<std::size_t> initial;
-  LightfieldFromFile<uint16_t> lightfield;
-
- public:
-  LFFromPPMSmallGreek()
-      : size(3, 3, 32, 32), initial(0, 0, 0, 0),
-        lightfield({{resources_path + "/small_greek/"}, initial, size}){};
-  ~LFFromPPMSmallGreek() = default;
-};
+/**
+ * \todo Update to consider PGX files...
+ */
 
 
-TEST_F(LFFromPPMSmallGreek, HasCorrectWidth) {
-  auto width = lightfield.get_width();
-  EXPECT_EQ(width, 3);
-}
+// struct LFFromPPMSmallGreek : public testing::Test {
+//  protected:
+//   LightfieldDimension<std::size_t> size;
+//   LightfieldCoordinate<std::size_t> initial;
+//   LightfieldFromFile<uint16_t> lightfield;
+
+//  public:
+//   LFFromPPMSmallGreek()
+//       : size(3, 3, 32, 32), initial(0, 0, 0, 0),
+//         lightfield({{resources_path + "/small_greek/"}, initial, size}){};
+//   ~LFFromPPMSmallGreek() = default;
+// };
 
 
-TEST_F(LFFromPPMSmallGreek, HasCorrectHeight) {
-  auto height = lightfield.get_height();
-  EXPECT_EQ(height, 3);
-}
+// TEST_F(LFFromPPMSmallGreek, HasCorrectWidth) {
+//   auto width = lightfield.get_width();
+//   EXPECT_EQ(width, 3);
+// }
 
 
-TEST_F(LFFromPPMSmallGreek, HasCorrectViewWidth) {
-  auto width = lightfield.get_views_width_u();
-  EXPECT_EQ(width, 32);
-}
+// TEST_F(LFFromPPMSmallGreek, HasCorrectHeight) {
+//   auto height = lightfield.get_height();
+//   EXPECT_EQ(height, 3);
+// }
 
 
-TEST_F(LFFromPPMSmallGreek, HasCorrectViewHeight) {
-  auto height = lightfield.get_views_height_v();
-  EXPECT_EQ(height, 32);
-}
+// TEST_F(LFFromPPMSmallGreek, HasCorrectViewWidth) {
+//   auto width = lightfield.get_views_width_u();
+//   EXPECT_EQ(width, 32);
+// }
 
 
-TEST_F(LFFromPPMSmallGreek, HasCorrectViewBpp) {
-  //notice that there is no bpp informed
-  auto bpp = lightfield.get_views_bpp();
-  EXPECT_EQ(bpp, 10);
-}
+// TEST_F(LFFromPPMSmallGreek, HasCorrectViewHeight) {
+//   auto height = lightfield.get_views_height_v();
+//   EXPECT_EQ(height, 32);
+// }
 
 
-TEST_F(LFFromPPMSmallGreek, HasCorrectDimensions) {
-  auto dimensions = lightfield.get_dimensions();
-  EXPECT_EQ(dimensions, size);
-}
+// TEST_F(LFFromPPMSmallGreek, HasCorrectViewBpp) {
+//   //notice that there is no bpp informed
+//   auto bpp = lightfield.get_views_bpp();
+//   EXPECT_EQ(bpp, 10);
+// }
 
 
-TEST_F(LFFromPPMSmallGreek, ZeroZeroImageIsEqual) {
-  auto file_original =
-      PixelMapFileIO::open({resources_path + "/small_greek/000_000.ppm"});
-  auto variadic_image = file_original->read_full_image();
-  auto image = std::move(std::get<1>(variadic_image));
-  EXPECT_EQ(*image, lightfield.get_image_at({0, 0}));
-}
+// TEST_F(LFFromPPMSmallGreek, HasCorrectDimensions) {
+//   auto dimensions = lightfield.get_dimensions();
+//   EXPECT_EQ(dimensions, size);
+// }
 
 
-TEST_F(LFFromPPMSmallGreek, ZeroZeroImageIsDifferentFromOneOneImage) {
-  auto file_original =
-      PixelMapFileIO::open({resources_path + "/small_greek/001_001.ppm"});
-  auto variadic_image = file_original->read_full_image();
-  auto image = std::move(std::get<1>(variadic_image));
-  EXPECT_NE(*image, lightfield.get_image_at({0, 0}));
-}
+// TEST_F(LFFromPPMSmallGreek, ZeroZeroImageIsEqual) {
+//   auto file_original =
+//       PixelMapFileIO::open({resources_path + "/small_greek/000_000.ppm"});
+//   auto variadic_image = file_original->read_full_image();
+//   auto image = std::move(std::get<1>(variadic_image));
+//   EXPECT_EQ(*image, lightfield.get_image_at({0, 0}));
+// }
 
 
-TEST_F(LFFromPPMSmallGreek, OneOneImageIsEqual) {
-  auto file_original =
-      PixelMapFileIO::open({resources_path + "/small_greek/001_001.ppm"});
-  auto variadic_image = file_original->read_full_image();
-  auto image = std::move(std::get<1>(variadic_image));
-  EXPECT_EQ(*image, lightfield.get_image_at({1, 1}));
-}
+// TEST_F(LFFromPPMSmallGreek, ZeroZeroImageIsDifferentFromOneOneImage) {
+//   auto file_original =
+//       PixelMapFileIO::open({resources_path + "/small_greek/001_001.ppm"});
+//   auto variadic_image = file_original->read_full_image();
+//   auto image = std::move(std::get<1>(variadic_image));
+//   EXPECT_NE(*image, lightfield.get_image_at({0, 0}));
+// }
 
 
-TEST_F(LFFromPPMSmallGreek, AllImagesAreEqualTheirExpectedImage) {
-  PPM3CharViewToFilename filename_translator;
-  for (auto t = 0; t < 3; ++t) {
-    for (auto s = 0; s < 3; ++s) {
-      auto file_original = PixelMapFileIO::open(
-          {resources_path + "/small_greek/" +
-              filename_translator.view_position_to_filename({t, s})});
-      auto variadic_image = file_original->read_full_image();
-      auto image = std::move(std::get<1>(variadic_image));
-      EXPECT_EQ(*image, lightfield.get_image_at({t, s}));
-    }
-  }
-}
+// TEST_F(LFFromPPMSmallGreek, OneOneImageIsEqual) {
+//   auto file_original =
+//       PixelMapFileIO::open({resources_path + "/small_greek/001_001.ppm"});
+//   auto variadic_image = file_original->read_full_image();
+//   auto image = std::move(std::get<1>(variadic_image));
+//   EXPECT_EQ(*image, lightfield.get_image_at({1, 1}));
+// }
 
 
-TEST_F(LFFromPPMSmallGreek,
-    TranposingTheIndicesGetsWrongImagesButTheMainDiagonal) {
-  PPM3CharViewToFilename filename_translator;
-  for (auto t = 0; t < 3; ++t) {
-    for (auto s = 0; s < 3; ++s) {
-      auto file_original = PixelMapFileIO::open(
-          {resources_path + "/small_greek/" +
-              filename_translator.view_position_to_filename({s, t})});
-      auto variadic_image = file_original->read_full_image();
-      auto image = std::move(std::get<1>(variadic_image));
-      if (s == t) {
-        EXPECT_EQ(*image, lightfield.get_image_at({t, s}));
-      } else {
-        EXPECT_NE(*image, lightfield.get_image_at({t, s}));
-      }
-    }
-  }
-}
+// TEST_F(LFFromPPMSmallGreek, AllImagesAreEqualTheirExpectedImage) {
+//   PPM3CharViewToFilename filename_translator;
+//   for (auto t = 0; t < 3; ++t) {
+//     for (auto s = 0; s < 3; ++s) {
+//       auto file_original = PixelMapFileIO::open(
+//           {resources_path + "/small_greek/" +
+//               filename_translator.view_position_to_filename({t, s})});
+//       auto variadic_image = file_original->read_full_image();
+//       auto image = std::move(std::get<1>(variadic_image));
+//       EXPECT_EQ(*image, lightfield.get_image_at({t, s}));
+//     }
+//   }
+// }
+
+
+// TEST_F(LFFromPPMSmallGreek,
+//     TranposingTheIndicesGetsWrongImagesButTheMainDiagonal) {
+//   PPM3CharViewToFilename filename_translator;
+//   for (auto t = 0; t < 3; ++t) {
+//     for (auto s = 0; s < 3; ++s) {
+//       auto file_original = PixelMapFileIO::open(
+//           {resources_path + "/small_greek/" +
+//               filename_translator.view_position_to_filename({s, t})});
+//       auto variadic_image = file_original->read_full_image();
+//       auto image = std::move(std::get<1>(variadic_image));
+//       if (s == t) {
+//         EXPECT_EQ(*image, lightfield.get_image_at({t, s}));
+//       } else {
+//         EXPECT_NE(*image, lightfield.get_image_at({t, s}));
+//       }
+//     }
+//   }
+// }
 
 
 int main(int argc, char *argv[]) {
