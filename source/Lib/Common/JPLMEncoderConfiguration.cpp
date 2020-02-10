498202273b52c563667fbf46a089f296b6178750
Pedro Garcia Freitas <pdr.grc.frts@gmail.com>
2020-02-10 15:56:46 -0300

    Fix source code format of Lib/Common/JPLMEncoderConfiguration.cpp file.

--- source/Lib/Common/JPLMEncoderConfiguration.cpp
+++ source/Lib/Common/JPLMEncoderConfiguration.cpp
@@ -43,7 +43,7 @@
 
 JPLMEncoderConfiguration::JPLMEncoderConfiguration(int argc, char **argv)
     : JPLMEncoderConfiguration(
-    argc, argv, JPLMEncoderConfiguration::current_hierarchy_level) {
+          argc, argv, JPLMEncoderConfiguration::current_hierarchy_level) {
   run_help();
 }
 
@@ -60,7 +60,7 @@ void JPLMEncoderConfiguration::parse_jpeg_pleno_part(const json &conf) {
   if (conf.contains("part")) {
     string p = conf["part"].get<string>();
     std::transform(p.begin(), p.end(), p.begin(),
-                   [](unsigned char c) { return std::tolower(c); });
+        [](unsigned char c) { return std::tolower(c); });
     if (p == "part 2" || p == "part2" || p == "part_2" || p == "light_fields")
       part = JpegPlenoPart::LightField;
   }
@@ -73,7 +73,7 @@ void JPLMEncoderConfiguration::parse_colorspace(const json &conf) {
   if (conf.contains("colorspace")) {
     string c = conf["colorspace"].get<string>();
     std::transform(c.begin(), c.end(), c.begin(),
-                   [](unsigned char c) { return std::tolower(c); });
+        [](unsigned char c) { return std::tolower(c); });
     if (c == "ycbcr" || c == "bt601")
       colorspace = ColorSpaces::ColorSpace::BT601;
     else if (c == "rgb")
@@ -86,7 +86,7 @@ void JPLMEncoderConfiguration::parse_colorspace(const json &conf) {
       colorspace = ColorSpaces::ColorSpace::YCoCg;
     else
       throw JPLMConfigurationExceptions::
-      NotImplementedYetInputTypeParseException(c);
+          NotImplementedYetInputTypeParseException(c);
   }
 }
 
@@ -105,12 +105,12 @@ ColorSpaces::ColorSpace JPLMEncoderConfiguration::get_colorspace() const {
   return colorspace;
 }
 
-JPLMEncoderConfiguration::JPLMEncoderConfiguration(int argc, char **argv,
-                                                   std::size_t level)
+JPLMEncoderConfiguration::JPLMEncoderConfiguration(
+    int argc, char **argv, std::size_t level)
     : JPLMConfiguration(argc, argv, level) {
   add_options_to_cli();
-  this->message = "JPLM Encoder\nUsage: " + this->executable_name +
-                  " [OPTIONS]\nOptions: ";
+  this->message =
+      "JPLM Encoder\nUsage: " + this->executable_name + " [OPTIONS]\nOptions: ";
 
 
   this->parse_cli(argc, argv);
@@ -118,41 +118,41 @@ JPLMEncoderConfiguration::JPLMEncoderConfiguration(int argc, char **argv,
 
 void JPLMEncoderConfiguration::add_options_to_cli() {
   cli_options.push_back({"--input", "-i",
-                         "Input directory containing the plenoptic data to be compressed "
-                         "(according to the JPEG Pleno Part). "
-                         "\n\tFor Part 2, light field, the input is a directory containing a "
-                         "set of directories (one for each color channel). Each one of those "
-                         "directories contains a set of views in PGX format.",
-                         [this]([[maybe_unused]] std::any v) {
-                           this->input = std::any_cast<std::string>(v);
-                         },
-                         this->current_hierarchy_level});
+      "Input directory containing the plenoptic data to be compressed "
+      "(according to the JPEG Pleno Part). "
+      "\n\tFor Part 2, light field, the input is a directory containing a "
+      "set of directories (one for each color channel). Each one of those "
+      "directories contains a set of views in PGX format.",
+      [this]([[maybe_unused]] std::any v) {
+        this->input = std::any_cast<std::string>(v);
+      },
+      this->current_hierarchy_level});
   cli_options.push_back({"--output", "-o",
-                         "Output, i.e., the compressed JPEG Pleno bitstream (filename.jpl).",
-                         [this]([[maybe_unused]] std::any v) {
-                           this->output = std::any_cast<std::string>(v);
-                         },
-                         this->current_hierarchy_level});
+      "Output, i.e., the compressed JPEG Pleno bitstream (filename.jpl).",
+      [this]([[maybe_unused]] std::any v) {
+        this->output = std::any_cast<std::string>(v);
+      },
+      this->current_hierarchy_level});
 
   //<! \todo check if the config should be placed in the JPLMConfiguration (to allow its use in the decoder)
   cli_options.push_back({"--config", "-c", "Path to config file",
-                         [this](std::any v) {
-                           this->config = std::any_cast<std::string>(v);
-                           if (!this->config.empty()) {
-                             if (fs::exists(this->config)) {
-                               parse_json(this->config);
-                             } else {
-                               throw JPLMConfigurationExceptions::
-                               ConfigFileDoesNotExistException(this->config);
-                             }
-                           }
-                         },
-                         this->current_hierarchy_level});
+      [this](std::any v) {
+        this->config = std::any_cast<std::string>(v);
+        if (!this->config.empty()) {
+          if (fs::exists(this->config)) {
+            parse_json(this->config);
+          } else {
+            throw JPLMConfigurationExceptions::ConfigFileDoesNotExistException(
+                this->config);
+          }
+        }
+      },
+      this->current_hierarchy_level});
 
-  cli_options.push_back({"--part", "-p",
-                         "enum/JpegPlenoPart in { LightField=2 }",
-                         [this](std::any value) {
-                           this->part = JpegPlenoPart{std::stoi(std::any_cast<string>(value))};
-                         },
-                         this->current_hierarchy_level});
+  cli_options.push_back(
+      {"--part", "-p", "enum/JpegPlenoPart in { LightField=2 }",
+          [this](std::any value) {
+            this->part = JpegPlenoPart{std::stoi(std::any_cast<string>(value))};
+          },
+          this->current_hierarchy_level});
 }
