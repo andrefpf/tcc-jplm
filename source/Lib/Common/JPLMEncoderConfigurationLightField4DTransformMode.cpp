e7256ae42d93f64fdfe55d15d2c0895bf21d920c
Ismael Seidel <ismaelseidel@gmail.com>
2020-02-13 14:56:21 -0300

    add cli/json for transform_size_maximum_intra_view_vertical

--- source/Lib/Common/JPLMEncoderConfigurationLightField4DTransformMode.cpp
+++ source/Lib/Common/JPLMEncoderConfigurationLightField4DTransformMode.cpp
@@ -64,8 +64,19 @@ void JPLMEncoderConfigurationLightField4DTransformMode::add_options_to_cli() {
       {[this]() { this->maximal_transform_size_inter_view_horizontal_s = 13; },
           "Default: 13"}});
 
-  cli_options.push_back({"--transform_size_maximum_intra_view_vertical",
+  this->add_cli_json_option({"--transform_size_maximum_intra_view_vertical",
       "-TMiv", "Maximum 4D transform size in intra-view vertical direction.",
+      [this](const json &conf) -> std::optional<std::string> {
+        try {
+          return std::to_string(conf.at("transform_size")
+                                    .at("maximum")
+                                    .at("intra-view")
+                                    .at("vertical")
+                                    .get<uint32_t>());
+        } catch (json::out_of_range &e) {
+        }
+        return std::nullopt;
+      },
       [this](std::string arg) {
         this->maximal_transform_size_intra_view_vertical_v =
             static_cast<uint32_t>(std::stoul(arg));
