60e587b5e3250d69d1bb9b97bc7bbccd74a9b86f
Ismael Seidel <ismaelseidel@gmail.com>
2020-02-13 14:18:18 -0300

    add default transform_size_maximum_inter_view_vertical=13

--- source/Lib/Common/JPLMEncoderConfigurationLightField4DTransformMode.cpp
+++ source/Lib/Common/JPLMEncoderConfigurationLightField4DTransformMode.cpp
@@ -51,7 +51,9 @@ void JPLMEncoderConfigurationLightField4DTransformMode::add_options_to_cli() {
         this->maximal_transform_size_inter_view_vertical_t =
             static_cast<uint32_t>(std::stoul(typed_string));
       },
-      this->current_hierarchy_level});
+      this->current_hierarchy_level,
+      {[this]() { this->maximal_transform_size_inter_view_vertical_t = 13; },
+          "Default: 13"}});
 
   cli_options.push_back({"--transform_size_maximum_inter_view_horizontal",
       "-TMIh", "Maximum 4D transform size in inter-view horizontal direction.",
