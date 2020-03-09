595532ad70d00c3a0c748d9026fa88bb126a66b6
Ismael Seidel <ismaelseidel@gmail.com>
2020-03-09 11:37:14 -0300

    added round back to each element of transformed block atfter 1d transform

--- source/Lib/Part2/Common/TransformMode/Transformed4DBlock.h
+++ source/Lib/Part2/Common/TransformMode/Transformed4DBlock.h
@@ -110,7 +110,7 @@ class Transformed4DBlock {
           for (decltype(max_c) d = 0; d < max_d; ++d) {
             //when std::transform_reduce is available, it should be possible to
             //just swap std::inner_product by std::transform_reduce
-            *dest_ptr =
+            *dest_ptr = std::round(
                 weight *
                 std::inner_product(
                     temp_initial,  //initial position of the original array
@@ -118,7 +118,7 @@ class Transformed4DBlock {
                     coefficients +
                         d * max_d,  //initial position of the multiplication array (coefficients array)
                     0.0  //initial sum value
-                );
+                    ));
             dest_ptr += stride_d;
           }
           stride += stride_c;
