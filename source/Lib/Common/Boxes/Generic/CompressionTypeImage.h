f1aeb374fe7ffc734d30d21617348f54e4f64243
Ismael Seidel <ismaelseidel@gmail.com>
2020-03-24 18:08:38 -0300

    updated compression image type (C field of Image header box) with correct values from ISO/IEC 15444-2:2004 and ISO/IEX 15444-2:2004/AMD 3

--- source/Lib/Common/Boxes/Generic/CompressionTypeImage.h
+++ source/Lib/Common/Boxes/Generic/CompressionTypeImage.h
@@ -43,18 +43,34 @@
 
 #include "CompressionType.h"
 
+
 /**
  * \brief      Enum class for representing the compression type of images.
  * \note       The values that are not defined in the present enum are reserved for ISO use.
  */
 enum class CompressionTypeImage : compression_type_data {
-  JPEG_2000 = 0, //!< Indicates that <a href="https://jpeg.org/jpeg2000/">JPEG 2000</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 0
-  JPEG = 1, //!< Indicates that <a href="https://jpeg.org/jpeg/index.html">JPEG</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 1
-  JPEG_LS = 2, //!< Indicates that <a href="https://jpeg.org/jpegls/index.html">JPEG LS</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 2
-  JPEG_XT = 3, //!< Indicates that <a href="https://jpeg.org/jpegxt/index.html">JPEG XT</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 3
-  JPEG_XR = 4, //!< Indicates that <a href="https://jpeg.org/jpegxr/index.html">JPEG XR</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 4
-  JPEG_XS = 5 //!< Indicates that <a href="https://jpeg.org/jpegxs/index.html">JPEG XS</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 5
-  //all other values are reserved for ISO use
+  // ISO/IEC 15444-2:2004 defines the following values:
+  Uncompressed = 0,
+  Modified_Huffman = 1,
+  Modified_READ = 2,
+  Modified_Modified_READ = 3,
+  JBIG_bilevel = 4,
+  JPEG =
+      5,  //!< Indicates that <a href="https://jpeg.org/jpeg/index.html">JPEG</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 5
+  JPEG_LS =
+      6,  //!< Indicates that <a href="https://jpeg.org/jpegls/index.html">JPEG LS</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 6
+  JPEG_2000 =
+      7,  //!< Indicates that <a href="https://jpeg.org/jpeg2000/">JPEG 2000</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 0
+  JBIG2 = 8,
+  JBIG_any = 9,
+  // ISO/IEC 15444-2:2004/AMD 3 defines the following values:
+  RLE = 10,
+  JPEG_XR =
+      11,  //!< Indicates that <a href="https://jpeg.org/jpegxr/index.html">JPEG XR</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 11
 };
+
+//\todo find and include the values for JPEG_XT and JPEG_XS
+//XS comment: Indicates that <a href="https://jpeg.org/jpegxs/index.html">JPEG XS</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 5
+//XT comment: Indicates that <a href="https://jpeg.org/jpegxt/index.html">JPEG XT</a> is used to encode the image. It is coded as \link compression_type_data \endlink value 3
 
 #endif /* end of include guard: JPLM_LIB_COMMON_BOXES_GENERIC_COMPRESSIONTYPEIMAGE_H__ */
