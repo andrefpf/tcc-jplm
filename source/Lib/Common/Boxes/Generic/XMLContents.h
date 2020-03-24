64328f8bc9f1dfdd1efd0171df9e066d8dbdaaea
Ismael Seidel <ismaelseidel@gmail.com>
2020-03-24 10:39:28 -0300

    added documentation to XMLContents

--- source/Lib/Common/Boxes/Generic/XMLContents.h
+++ source/Lib/Common/Boxes/Generic/XMLContents.h
@@ -45,49 +45,119 @@
 #include "Lib/Common/Boxes/InMemoryDBox.h"
 #include "Lib/Utils/Stream/BinaryTools.h"
 
+
 class XMLContents : public InMemoryDBox {
  protected:
-  std::string contents;
+  std::string contents = "";
 
  public:
+  /**
+   * @brief      Constructs a new instance having empty content.
+   */
   XMLContents() = default;
 
 
+  /**
+   * @brief      Constructs a new instance by copying the contents string.
+   *
+   * @param[in]  contents  The contents
+   */
   XMLContents(const std::string& contents) : contents(contents) {
   }
 
 
+  /**
+   * @brief      Constructs a new instance by obtaining the ownership of the contents string.
+   *
+   * @param      contents  The contents
+   */
   XMLContents(std::string&& contents) : contents(std::move(contents)) {
   }
 
 
+  /**
+   * @brief      Creates a new instance of the object with same properties than original.
+   *
+   * @return     Copy of this object.
+   */
   XMLContents* clone() const override;
 
 
+  /**
+   * @brief      Destroys the object.
+   */
   virtual ~XMLContents() = default;
 
 
+  /**
+   * @brief      Gets the size of the XML contents (in bytes)
+   *
+   * @return     Size (in bytes)
+   */
   uint64_t size() const noexcept override;
 
 
+  /**
+   * @brief      Determines whether the specified other is equal.
+   *
+   * @param[in]  other  The other
+   *
+   * @return     True if the specified other is equal, False otherwise.
+   */
   bool is_equal(const DBox& other) const override;
 
 
+  /**
+   * @brief      Equality operator.
+   *
+   * @param[in]  other  The other
+   *
+   * @return     The result of the equality
+   */
   bool operator==(const XMLContents& other) const;
 
 
+  /**
+   * @brief      Inequality operator.
+   *
+   * @param[in]  other  The other
+   *
+   * @return     The result of the inequality
+   */
   bool operator!=(const XMLContents& other) const;
 
 
+  /**
+   * @brief      Gets the string with contents.
+   *             
+   *             The XML contains basically a string with a XML code.
+   *             \todo Validate the XML contained in the string
+   * @return     The string with contents.
+   */
   const std::string& get_string_with_contents() const noexcept;
 
 
+  /**
+   * @brief      Gets the bytes.
+   *
+   * @return     The bytes.
+   */
   virtual std::vector<std::byte> get_bytes() const noexcept override;
 
 
+  /**
+   * @brief      Sets the contents by copy.
+   *
+   * @param[in]  new_content  The new content
+   */
   void set_contents(const std::string& new_content);
 
 
+  /**
+   * @brief      Sets the contents by assuming the ownership (move).
+   *
+   * @param      new_content  The new content
+   */
   void set_contents(std::string&& new_content);
 };
 
